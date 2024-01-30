a37bdab83f8482cfc3806ace4c56d955db2612ad
Ebrahim-Elkbbany <136932497+Ebrahim-Elkbbany@users.noreply.github.com>
2024-01-30 21:48:34 +0200

    - init App

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterAiApp());
+}
+
+class FlutterAiApp extends StatelessWidget {
+  const FlutterAiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
+
